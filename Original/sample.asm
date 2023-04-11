c55b1c924f2ff87a9b0ad971d5a449485b9112a2
Dhruv <Dhruvahlawat573@gmail.com>
2023-04-11 22:00:22 +0530

    79 starter code

--- Original/sample.asm
+++ Original/sample.asm
@@ -1,12 +1,4 @@
-main:
-	addi	$t0, $0, 10
-	add		$t1, $0, $0
-	addi	$s1, $zero, 320
-loop:
-	beq		$t1, $t0, exit
-	sw		$s0, 12($s1)
-	addi	$s0, $s0, 100
-	addi	$t1, $t1, 1
-	j		loop
-
-exit:
+addi $1, $0, 50
+sw $1, 2($1) 
+lw $2, 2($1)
+addi $3, $2, 50
